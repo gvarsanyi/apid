e3421fad63954d4cd8773be1becc0d828a63d329
Greg Varsanyi <gvarsanyi@gmail.com>
2015-01-07 12:04:28 -0800

    diverting server output immediately

--- coffee/lib/server.coffee
+++ coffee/lib/server.coffee
@@ -8,10 +8,72 @@ class Server extends Bridge
   jotServer: null # jotServer
 
 
-  constructor: ->
-    super
+  start: (name, options, cb) =>
+    unless typeof options is 'object'
+      [cb, options] = [options, {}]
+    @setConfig name, options
 
-  divertExit: =>
+    @jotServer = jot.createServer @socketFile
+
+    @jotServer.on 'listening', =>
+      console.log 'server listening on:', @socketFile
+      @readyFlush cb
+
+    @jotServer.on 'connection', (@socket) =>
+      @revealExposed()
+
+      socket.on 'data', (data) =>
+        # console.log 'data', data
+        if data.api
+          @attachRemote data
+          socket.write ack: 1
+        else if data.req
+          @request data.req
+        else
+          @response data?.res
+
+    @jotServer.listen @socketFile
+
+
+module.exports = Server
+
+
+# Server only: divert STDOUT/STDERR to files and hijack exit processes
+# Output files: ~/.config/<ID>/apid.out and ~/.config/<ID>/apid.err
+do ->
+  fn = process.mainModule.filename
+  if fn.indexOf('node_modules/daemonize2/lib/wrapper.js') is fn.length - 38
+    name = process.title
+
+    home = process.env.HOME
+    if process.platform is 'win32'
+      home = process.env.USERPROFILE
+
+    unless name and home
+      throw new Error 'APID can not find environment values for home|name: ' +
+                      home + '|' + name
+
+    config_path = home + '/.config/' + name
+
+    require('mkdirp').sync config_path
+
+    # divert stdout/stderr
+    file = config_path + '/apid-' + process.getuid() + '.'
+    opts = {encoding: 'utf8', flags: 'a'}
+    std_streams = {}
+    for type in ['err', 'out']
+      do (type) ->
+        process['std' + type].write = (d) ->
+          unless std_streams[type]
+            std_streams[type] = fs.createWriteStream file + type, opts
+            std_streams[type].once 'close', ->
+              std_streams[type] = null
+            std_streams[type].once 'error', ->
+              std_streams[type] = null
+
+          std_streams[type].write d
+
+    # divert exit processes
     cleaned_up = false
     for event in ['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGBREAK', 'exit',
                   'uncaughtException']
@@ -25,58 +87,5 @@ class Server extends Bridge
               cleaned_up = true
           unless event is 'exit'
             process.exit 0
+
     return
-
-  divertStdOutput: (cb) =>
-    mkdirp = require 'mkdirp'
-    mkdirp @configPath, (err) =>
-      return cb(err) if err
-
-      file = @configPath + '/apid-' + process.getuid() + '.'
-      opts = {encoding: 'utf8', flags: 'a'}
-      std_streams = {}
-      for type in ['err', 'out']
-        do (type) ->
-          process['std' + type].write = (d) ->
-            unless std_streams[type]
-              std_streams[type] = fs.createWriteStream file + type, opts
-              std_streams[type].once 'close', ->
-                std_streams[type] = null
-              std_streams[type].once 'error', ->
-                std_streams[type] = null
-
-            std_streams[type].write d
-      cb()
-
-  start: (name, options, cb) =>
-    unless typeof options is 'object'
-      [cb, options] = [options, {}]
-    @setConfig name, options
-#     process.title = @name
-
-    @divertExit()
-    @divertStdOutput =>
-
-      @jotServer = jot.createServer @socketFile
-
-      @jotServer.on 'listening', =>
-        console.log 'server listening on:', @socketFile
-        @readyFlush cb
-
-      @jotServer.on 'connection', (@socket) =>
-        @revealExposed()
-
-        socket.on 'data', (data) =>
-          # console.log 'data', data
-          if data.api
-            @attachRemote data
-            socket.write ack: 1
-          else if data.req
-            @request data.req
-          else
-            @response data?.res
-
-      @jotServer.listen @socketFile
-
-
-module.exports = Server
--- js/lib/server.js
+++ js/lib/server.js
@@ -13,20 +13,95 @@ Bridge = require('./bridge');
 Server = (function(_super) {
   __extends(Server, _super);
 
-  Server.prototype.jotServer = null;
-
   function Server() {
     this.start = __bind(this.start, this);
-    this.divertStdOutput = __bind(this.divertStdOutput, this);
-    this.divertExit = __bind(this.divertExit, this);
-    Server.__super__.constructor.apply(this, arguments);
+    return Server.__super__.constructor.apply(this, arguments);
   }
 
-  Server.prototype.divertExit = function() {
-    var cleaned_up, event, _fn, _i, _len, _ref;
+  Server.prototype.jotServer = null;
+
+  Server.prototype.start = function(name, options, cb) {
+    var _ref;
+    if (typeof options !== 'object') {
+      _ref = [options, {}], cb = _ref[0], options = _ref[1];
+    }
+    this.setConfig(name, options);
+    this.jotServer = jot.createServer(this.socketFile);
+    this.jotServer.on('listening', (function(_this) {
+      return function() {
+        console.log('server listening on:', _this.socketFile);
+        return _this.readyFlush(cb);
+      };
+    })(this));
+    this.jotServer.on('connection', (function(_this) {
+      return function(socket) {
+        _this.socket = socket;
+        _this.revealExposed();
+        return socket.on('data', function(data) {
+          if (data.api) {
+            _this.attachRemote(data);
+            return socket.write({
+              ack: 1
+            });
+          } else if (data.req) {
+            return _this.request(data.req);
+          } else {
+            return _this.response(data != null ? data.res : void 0);
+          }
+        });
+      };
+    })(this));
+    return this.jotServer.listen(this.socketFile);
+  };
+
+  return Server;
+
+})(Bridge);
+
+module.exports = Server;
+
+(function() {
+  var cleaned_up, config_path, event, file, fn, home, name, opts, std_streams, type, _fn, _fn1, _i, _j, _len, _len1, _ref, _ref1;
+  fn = process.mainModule.filename;
+  if (fn.indexOf('node_modules/daemonize2/lib/wrapper.js') === fn.length - 38) {
+    name = process.title;
+    home = process.env.HOME;
+    if (process.platform === 'win32') {
+      home = process.env.USERPROFILE;
+    }
+    if (!(name && home)) {
+      throw new Error('APID can not find environment values for home|name: ' + home + '|' + name);
+    }
+    config_path = home + '/.config/' + name;
+    require('mkdirp').sync(config_path);
+    file = config_path + '/apid-' + process.getuid() + '.';
+    opts = {
+      encoding: 'utf8',
+      flags: 'a'
+    };
+    std_streams = {};
+    _ref = ['err', 'out'];
+    _fn = function(type) {
+      return process['std' + type].write = function(d) {
+        if (!std_streams[type]) {
+          std_streams[type] = fs.createWriteStream(file + type, opts);
+          std_streams[type].once('close', function() {
+            return std_streams[type] = null;
+          });
+          std_streams[type].once('error', function() {
+            return std_streams[type] = null;
+          });
+        }
+        return std_streams[type].write(d);
+      };
+    };
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      type = _ref[_i];
+      _fn(type);
+    }
     cleaned_up = false;
-    _ref = ['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGBREAK', 'exit', 'uncaughtException'];
-    _fn = (function(_this) {
+    _ref1 = ['SIGINT', 'SIGTERM', 'SIGHUP', 'SIGBREAK', 'exit', 'uncaughtException'];
+    _fn1 = (function(_this) {
       return function(event) {
         return process.on(event, function() {
           var args;
@@ -45,88 +120,9 @@ Server = (function(_super) {
         });
       };
     })(this);
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      event = _ref[_i];
-      _fn(event);
+    for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+      event = _ref1[_j];
+      _fn1(event);
     }
-  };
-
-  Server.prototype.divertStdOutput = function(cb) {
-    var mkdirp;
-    mkdirp = require('mkdirp');
-    return mkdirp(this.configPath, (function(_this) {
-      return function(err) {
-        var file, opts, std_streams, type, _fn, _i, _len, _ref;
-        if (err) {
-          return cb(err);
-        }
-        file = _this.configPath + '/apid-' + process.getuid() + '.';
-        opts = {
-          encoding: 'utf8',
-          flags: 'a'
-        };
-        std_streams = {};
-        _ref = ['err', 'out'];
-        _fn = function(type) {
-          return process['std' + type].write = function(d) {
-            if (!std_streams[type]) {
-              std_streams[type] = fs.createWriteStream(file + type, opts);
-              std_streams[type].once('close', function() {
-                return std_streams[type] = null;
-              });
-              std_streams[type].once('error', function() {
-                return std_streams[type] = null;
-              });
-            }
-            return std_streams[type].write(d);
-          };
-        };
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          type = _ref[_i];
-          _fn(type);
-        }
-        return cb();
-      };
-    })(this));
-  };
-
-  Server.prototype.start = function(name, options, cb) {
-    var _ref;
-    if (typeof options !== 'object') {
-      _ref = [options, {}], cb = _ref[0], options = _ref[1];
-    }
-    this.setConfig(name, options);
-    this.divertExit();
-    return this.divertStdOutput((function(_this) {
-      return function() {
-        _this.jotServer = jot.createServer(_this.socketFile);
-        _this.jotServer.on('listening', function() {
-          console.log('server listening on:', _this.socketFile);
-          return _this.readyFlush(cb);
-        });
-        _this.jotServer.on('connection', function(socket) {
-          _this.socket = socket;
-          _this.revealExposed();
-          return socket.on('data', function(data) {
-            if (data.api) {
-              _this.attachRemote(data);
-              return socket.write({
-                ack: 1
-              });
-            } else if (data.req) {
-              return _this.request(data.req);
-            } else {
-              return _this.response(data != null ? data.res : void 0);
-            }
-          });
-        });
-        return _this.jotServer.listen(_this.socketFile);
-      };
-    })(this));
-  };
-
-  return Server;
-
-})(Bridge);
-
-module.exports = Server;
+  }
+})();
